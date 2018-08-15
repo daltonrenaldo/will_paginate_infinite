9158410c1899cdee351ee53e9caec1994c9dd15d
Renaldo Pierre-Louis <pirelouisd87@gmail.com>
2018-08-15 18:25:55 -0400

    Update will_paginate_infinite.js
    changing text to english, and increase trigger threshold

--- app/assets/javascripts/will_paginate_infinite.js
+++ app/assets/javascripts/will_paginate_infinite.js
@@ -2,10 +2,10 @@ jQuery(function() {
   if ($('.infinite-pagination').size() > 0) {
     $(window).on('scroll', function() {
       var more_posts_url = $('.infinite-pagination a.next_page').attr('href');
-      var bottom_distance = 20;
+      var bottom_distance = 200;
 
       if (more_posts_url && $(window).scrollTop() > $(document).height() - $(window).height() - bottom_distance) {
-        $('.infinite-pagination').html('Carregando...');
+        $('.infinite-pagination').html('Loading...');
         $.getScript(more_posts_url);
       }
     });
