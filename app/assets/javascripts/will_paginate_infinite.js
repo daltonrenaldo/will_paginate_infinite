404fb1b9c892232c98e4d70c3a84a4d874748fcc
Renaldo Pierre-Louis <pirelouisd87@gmail.com>
2018-08-17 11:19:03 -0400

    prevent multiple loading at once; one button pagination, cleaner code

--- app/assets/javascripts/will_paginate_infinite.js
+++ app/assets/javascripts/will_paginate_infinite.js
@@ -1,13 +1,28 @@
 jQuery(function() {
-  if ($('.infinite-pagination').size() > 0) {
-    $(window).on('scroll', function() {
-      var more_posts_url = $('.infinite-pagination a.next_page').attr('href');
-      var bottom_distance = 200;
+  $(window).on('scroll', function() {
+    if ($('.infinite-pagination').size() <= 0) return;
 
-      if (more_posts_url && $(window).scrollTop() > $(document).height() - $(window).height() - bottom_distance) {
-        $('.infinite-pagination').html('Loading...');
-        $.getScript(more_posts_url);
-      }
-    });
-  }
+    var bottom_distance;
+    var will_paginate_infinite_fetching = false;
+    var more_posts_url = $('.infinite-pagination a.next_page').attr('href');
+    var threshold = $('.infinite-pagination').data('trigger-threshold').toString();
+    var pixel_value = threshold.match(/(.*)(px)$/);
+
+    if (pixel_value) {
+      bottom_distance = pixel_value[1];
+    } else {
+      bottom_distance = ($(window).height() * threshold) / 100;
+    }
+
+    if (more_posts_url && $(window).scrollTop() > $(document).height() - $(window).height() - bottom_distance && !will_paginate_infinite_fetching) {
+      will_paginate_infinite_fetching = true;
+      $('.infinite-pagination').html('<div class="cp-spinner cp-round"></div>');
+      
+      $.getScript(more_posts_url).done(function () {
+        will_paginate_infinite_fetching = false;
+      }).fail(function (jqxhr, settings, exception) {
+        console.log(exception);
+      });
+    }
+  });
 });
--- app/helpers/infinite_helper.rb
+++ app/helpers/infinite_helper.rb
@@ -1,12 +1,11 @@
 module InfiniteHelper
   def infinite_append(containerSelector, render_options)
-    collection = render_options
-    collection = render_options[:collection] unless render_options.is_a?(ActiveRecord::Relation)
+    collection = render_options[:collection]
 
     html = "$('" + containerSelector + "').append('"+ j(render render_options) + "');"
 
     if collection.next_page
-      html += "$('.infinite-pagination').replaceWith('" + j(will_paginate(collection, renderer: WillPaginateInfinite::InfinitePagination)) + "');"
+      html += "$('.infinite-pagination').replaceWith('" + j(will_paginate(collection, render_options.merge({renderer: WillPaginateInfinite::InfinitePagination}) )) + "');"
     else
       html += "$(window).off('scroll');"
       html += "$('.infinite-pagination').remove();"
